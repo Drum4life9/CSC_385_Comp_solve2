65043d2bdfd865506ff8c9a93f68774a271187e1
Brian Myers <brian.myers@dm.org>
2025-08-27 10:08:54 -0400

    dvds done, ezpz

---
+++ DVDs/cmake-build-debug/CMakeFiles/DVDs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DVDs.dir/main.cpp.o"
+  "CMakeFiles/DVDs.dir/main.cpp.o.d"
+  "DVDs"
+  "DVDs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DVDs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- DVDs/main.cpp
+++ DVDs/main.cpp
@@ -4,45 +4,15 @@
 
 using namespace std;
 
-bool checkVectorSorted(const vector<int>& nums) {
-	if (nums.size() == 1) {
-		return true;
-	}
-
-	for (int i = 0; i < nums.size() - 1; i++) {
-		if (nums[i] + 1 != nums[i + 1]) {
-			return false;
-		}
-	}
-	return true;
-}
-
 int doDVDMoveFunction(int n, vector<int> v) {
-	int numMoves = 0;
-	int startIndexFromEnd = 0;
-	while (true) {
-		int lastNumMoves = 0;
-		int curNumber = v[v.size() - 1 - startIndexFromEnd];
-		for (int i = v.size() - 1; i >= 0; i--) {
-			if (v[i] == curNumber + 1) {
-				numMoves++;
-				lastNumMoves++;
-				curNumber = v[i];
-				auto it = v.begin() + i;
-				v.erase(it);
-				startIndexFromEnd = 0;
-				if (curNumber == n) {
-					return numMoves;
-				}
-			}
-		}
-		if (lastNumMoves == 0) {
-			startIndexFromEnd++;
-		}
-		if (startIndexFromEnd == v.size()) {
-			return numMoves;
+    int last_num_in_order = 0;
+	for (int i : v) {
+		if (i == last_num_in_order + 1) {
+			last_num_in_order = i;
 		}
 	}
+
+	return n - last_num_in_order;
 }
 
 int main() {
@@ -63,5 +33,10 @@ int main() {
 // 2
 // 4
 // 1 3 2 4
+// 5
+// 5 1 2 4 3
+// 2
+// 4
+// 4 3 2 1
 // 5
 // 5 1 2 4 3
