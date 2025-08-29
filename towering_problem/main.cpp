a699654cc56782829a5984c98db4bf3bc8bd2ce9
Brian Myers <brian.myers@dm.org>
2025-08-29 14:00:21 -0400

    Towering boxes mostly working

---
+++ towering_problem/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main() {
+    int t1_height, t2_height = 0;
+    vector<int> boxes(6);
+
+    for (int i = 0; i < 6; i++) {
+        cin >> boxes[i];
+    }
+    cin >> t1_height;
+    cin >> t2_height;
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = i+1; j < 5; j++) {
+            for (int k = j+1; k < 6; k++) {
+                int sum = boxes[i] + boxes[j] + boxes[k];
+                if (sum == t1_height || sum == t2_height) {
+                    int sum2 = 0;
+                    vector others = {-1, -1, -1};
+                    vector boxesUsing = {boxes[i], boxes[j], boxes[k]};
+                    int innerIndex = 0;
+                    for (int inner = 0; inner < 6; inner++) {
+                        if (inner != i && inner != j && inner != k) {
+                            sum2 += boxes[inner];
+                            others[innerIndex] = boxes[inner];
+                            innerIndex++;
+                        }
+                    }
+                    std::sort(others.begin(), others.end(), std::greater<int>());
+                    std::sort(boxesUsing.begin(), boxesUsing.end(), std::greater<int>());
+                    if ((sum == t1_height && sum2 == t2_height) || (sum == t2_height && sum2 == t1_height)) {
+                        cout << boxesUsing[0] << " " << boxesUsing[1] << " " << boxesUsing[2] << " " << others[0] << " " << others[1] << " " << others[2] << endl;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
